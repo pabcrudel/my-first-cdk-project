1bc862514b2656459c17a17d8c25ef6a982651c3
pabcrudel <pabcrudel@gmail.com>
2023-07-25 17:02:14 +0200

    Changed access policy

--- lib/website-stack.ts
+++ lib/website-stack.ts
@@ -2,7 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
+// import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class MyWebsite extends cdk.Stack {
@@ -12,44 +13,27 @@ export class MyWebsite extends cdk.Stack {
     // Creates the s3 bucket where the website will be hosted
     const webBucket = new s3.Bucket(this, 's3-hosting', {
       bucketName: "my-website-bucket-24-07-2023",
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: '404.html',
+      publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: '404.html',
     })
 
-    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'cloudfront-oai', {
-      comment: 'Allows CloudFront to reach the web bucket',
-    })
-
-    // Add a bucket policy to allow read access from CloudFront
-    webBucket.addToResourcePolicy(new iam.PolicyStatement({
-      actions: ['s3:GetObject'],
-      resources: [webBucket.bucketArn + '/*'],
-      principals: [originAccessIdentity.grantPrincipal],
-    }));
-
-    // Creates the cloudfront distribution that will be on top of the s3 bucket
-    const webDistribution = new cloudfront.CloudFrontWebDistribution(this, 'cloudfront-distribution', {
-      defaultRootObject: '/index.html',
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: webBucket,
-            originAccessIdentity: originAccessIdentity,
-          },
-          behaviors: [{ isDefaultBehavior: true }],
-        },
-      ],
-    })
+    const distribution = new cloudfront.Distribution(this, 'cloudfront-web-distribution', {
+      defaultBehavior: {
+        origin: new cloudfront_origins.S3Origin(webBucket) // This class automatically creates an Origin Access Identity
+      },
+    });
 
     // This function deploys the built files from the frontend to the s3 hosting the website
     new s3deploy.BucketDeployment(this, 's3-deploy', {
       sources: [s3deploy.Source.asset('./www'),],
       prune: false,
       destinationBucket: webBucket,
-      distribution: webDistribution,
-      distributionPaths: ['/index.html'],
+      distribution: distribution,
     })
   }
 }
